8825f194f4e92b2f9ebc950f8f6063fe1a6a6074
Carlos <100428991@alumnos.uc3m.es>
2021-09-27 18:55:07 +0200

    .

--- main.py
+++ main.py
@@ -13,3 +13,46 @@ class CryptoBank:
         # Si existe se le añade la cuenta
         # Si no, se crea de 0
         del josete
+
+    def delete_account(self, id, password, acc_name, key):
+        # Descargar la base de datos de las cuentas
+        # Comprobar si existe id/password
+        # Si existe buscamos la cuenta
+        # La borramos
+        # Si no, mandamos un warning
+        pass
+
+    def modify_account(self, id, password, acc_name, key):
+        # Descargar la base de datos de las cuentas
+        # Comprobar si existe id/password
+        # Si existe buscamos la cuenta
+        # La modificamos
+        # Si no, mandamos un warning
+        pass
+
+    def deposit(self, id, password, acc_name, key):
+        # Descargar la base de datos de las cuentas
+        # Comprobar si existe id/password
+        # Si existe buscamos la cuenta
+        # Sumamos el dinero
+        # Si no, mandamos un warning
+        pass
+
+    def withdraw(self, id, password, acc_name, key):
+        # Descargar la base de datos de las cuentas
+        # Comprobar si existe id/password
+        # Si existe buscamos la cuenta
+        # Restamos el dinero
+        # Si no, mandamos un warning
+        pass
+
+    def transfer(self, id, password, id2, acc_name, key, acc_name2):
+        # Descargar la base de datos de las cuentas
+        # Comprobar si existe id/password
+        # Si existe buscamos la cuenta
+        # Buscamos si existe id2
+        # Transferimos el dinero
+        # Si no, mandamos un warning
+        pass
+
+#En el futuro habrá métodos para quitar la redundancia de los métodos
