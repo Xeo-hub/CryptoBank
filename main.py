a2fdf260ef3abcc725c4efa09d4fbacd506e9874
ounsesa360 <78766066+ounsesa360@users.noreply.github.com>
2021-10-21 14:32:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 JSON_FILES_PATH = "D:/PyCharm/Proyectos/CryptoProject/JsonFiles/"
+from json_storage import JsonStore
 print(JSON_FILES_PATH)
 
 from users import User
@@ -6,20 +7,22 @@ from json_storage import JsonStore
 
 class CryptoBank:
     def create_account(self, id, password, acc_name, key):
-
         user = User(id, password)
         accounts_storage = JsonStore()
         accounts=accounts_storage.load_store()
-        account=accounts.find_item(user)
-        # Descargar la base de datos de las cuentas
-
+        account=self.verify(user)
         # Comprobar si existe id/password
-        # Si existe se le añade la cuenta
         # Si no, se crea de 0
-        del user
+        # Cifrar acc_name y key (esto puede)
+        if account==None:
+            accounts.add_item(user)
+        # Si existe se le mete en la cuenta
+
 
     def login(self, id, password):
-       self.verify (id, password)
+        self.verify (id, password)
+        print("Sesion iniciada")
+
 
     def verify (self,id, password:None):
         accounts_storage = JsonStore()
@@ -34,14 +37,15 @@ class CryptoBank:
         user = User(id, password)
         for item in accounts:
             if user in item.keys():
-                print("Sesión iniciada")
-                user.accounts=[]######
+                return user
         print("Error")
+        return None
 
 
     def delete_account(self, id, password, acc_name, key):
         # Descargar la base de datos de las cuentas
         # Comprobar si existe id/password
+        # Cifrar acc_name y key
         # Si existe buscamos la cuenta
         # La borramos
         # Si no, mandamos un warning
@@ -58,6 +62,7 @@ class CryptoBank:
     def deposit(self, id, password, acc_name, key):
         # Descargar la base de datos de las cuentas
         # Comprobar si existe id/password
+        # Ciframos acc_name key
         # Si existe buscamos la cuenta
         # Sumamos el dinero
         # Si no, mandamos un warning
@@ -66,6 +71,8 @@ class CryptoBank:
     def withdraw(self, id, password, acc_name, key):
         # Descargar la base de datos de las cuentas
         # Comprobar si existe id/password
+        # Ciframos acc_name key
+        # Autenticamos
         # Si existe buscamos la cuenta
         # Restamos el dinero
         # Si no, mandamos un warning
@@ -74,8 +81,11 @@ class CryptoBank:
     def transfer(self, id, password, id2, acc_name, key, acc_name2):
         # Descargar la base de datos de las cuentas
         # Comprobar si existe id/password
+        # Ciframos acc_name key y acc_name2
+        # Autenticamos
         # Si existe buscamos la cuenta
         # Buscamos si existe id2
+        # Firma digital
         # Transferimos el dinero
         # Si no, mandamos un warning
         pass
